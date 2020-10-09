71da6f46d7abd7e10d91cedf3b5592d0126de8ef
Davoleo <davoleo2001@gmail.com>
2020-10-09 17:37:33 +0200

    Add Nested if-else statements and while

--- theory/statements.cpp
+++ theory/statements.cpp
@@ -8,11 +8,12 @@ int main() {
     //Calcolo del valore assoluto
     int x;
     cin >> x;
-    if (x < 0)
+    if (x < 0) {
+        cout << "Il numero dato è negativo" << endl;
         x = -x;
-    cout << "Il valore assoluto è" << x << endl;
-    return 0;
-
+    }
+    cout << "Il valore assoluto è: " << x << endl;
+    
     //Massimo tra 2 numeri
     int y, max;
     cin >> x;
@@ -23,5 +24,30 @@ int main() {
         max = y;
     
     cout << max << endl;
-        
+
+    //Voto 2 Giudizio
+    float mark;
+    cout << "Scrivi un voto numerico (tra 0 e 10): ";
+    cin >> mark;
+
+    if  (mark < 0 || mark > 10)
+        cout << "Voto non Valido" << endl;
+    else if (mark <= 5)
+        cout << "Insufficiente" << endl;
+    else if (mark <= 6.5)
+        cout << "Sufficiente" << endl;
+    else if (mark <= 7.5)
+        cout << "Buono" << endl;
+    else 
+        cout << "Ottimo" << endl;
+
+    
+    //While Example
+    int i = 1;
+    while (i <= 10) {
+        cout << i * i << endl;
+        i++;
+    }
+
+    return 0;
 }
