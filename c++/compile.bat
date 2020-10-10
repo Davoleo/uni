73543fa091f1b0feede046fc938e75933a7ac065
Davoleo <davoleo2001@gmail.com>
2020-10-10 14:18:06 +0200

    Update compilation script

--- c++/compile.bat
+++ c++/compile.bat
@@ -1,1 +1,7 @@
-g++ ./theory/statements.cpp -o output
+::Coded by Davoleo::
+
+:: Compile the averager source file
+g++ ./theory/media.cpp -o ./build/media
+
+:: Compile the statement theory file
+g++ ./theory/statements.cpp -o ./build/statements
