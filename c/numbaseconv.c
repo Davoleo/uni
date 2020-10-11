0ee18be2ff2263259d815c9206a7ffcc98f9b41b
Davoleo <davoleo2001@gmail.com>
2020-10-11 19:17:28 +0200

    More C Hell | Trying to make nbc work

--- c/numbaseconv.c
+++ c/numbaseconv.c
@@ -6,13 +6,13 @@ const char alpha[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
 const int charToValueShift = 54;
 const int numToValueshift = 48;
 
-int customToDecimal(const char* number, int base);
-const char* decimalToCustom(int decimal, int base);
+int customToDecimal(const char number[], int base);
+void decimalToCustom(int decimal, int base, char* converted[]);
 
 int main() {
 
     int base1;
-    const char* number;
+    char number[256];
 
     puts("=========================");
     puts("| Number Base Converter |");
@@ -26,7 +26,7 @@ int main() {
     if (base1 < 2 || base1 > 37) 
     {
         puts("Base can't be smaller than binary or bigger than 37, the program will terminate now");
-        getchar();
+        system("pause");
         return 0;
     }
     
@@ -35,7 +35,7 @@ int main() {
     else
         printf("You can use digits from 0 to 9 and letters from A to %c to describe your number: ", alpha[base1 - 11]);
     
-    scanf("%s", &number);
+    scanf("%s", number);
 
     int base2;
     printf("Please insert the base you want to convert your nunber to [2, 37]: ");
@@ -45,31 +45,30 @@ int main() {
 
     printf("Decimal number is: %d", decimalNumber);
 
-    const char* finalNumber = decimalToCustom(decimalNumber, base2);
+    char finalNumber[256] = "";
 
-    printf("The final converted number is: %s", finalNumber);
+    decimalToCustom(decimalNumber, base2, &finalNumber);
+
+    printf("The final converted number is: %s", *finalNumber);
 
     return 0;
 }
 
-int customToDecimal(const char* number, int base) 
+int customToDecimal(const char number[], int base) 
 {
     int decimalSum = 0;
 
-    for (int i = 0; strlen(number); i++) {
+    for (int i = 0; i < strlen(number); i++) 
+    {
         if (number[i] <= 57) //the char is a normal digit
         {
             int converted = (number[i] - numToValueshift) * base;
             decimalSum += converted;
-            puts("1");
         }
-
-        puts("djaskkldjaklsdjlkd");
 
         if (number[i] >= 65) //The char is an extended digit from the alpha array
         {
             int converted = (number[i] - charToValueShift) * base;
-            puts("2");
             decimalSum += converted;
         }
     }
@@ -77,20 +76,35 @@ int customToDecimal(const char* number, int base)
     return decimalSum;
 }
 
-const char* decimalToCustom(int decimal, int base)
+void decimalToCustom(int decimal, int base, char* converted[])
 {
-    char* converted = "";
+    puts("se non printo io mi ammazzo");
+    int i = 0;
 
-    while (decimal != 0) {
+    while(decimal != 0) {
+
+        puts("Appena entrato nella balena");
+
         int result = decimal % base;
 
-        if (result > 9)
-            strcat(converted, alpha[result - 10]);
-        else
-            strcat(converted, result);
+        puts("Dopo aver preso il resto");
+        
+        if (result > 9) {
+            puts("if");
+            *converted[i] = alpha[result - 10];
+        }
+        else {
+            puts("else");
+            printf("%s", *converted);
+            system("pause");
+            //*converted[i] = result + numToValueshift;
+        }
 
+        puts("Dopo l'if");
+
+        i++;
         decimal /= base;
-    }
 
-    return converted;
+        puts("I mean, se stampa questo, io sono felice");
+    }
 }
