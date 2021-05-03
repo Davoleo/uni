e25607464e569c37c35fd2d53ead9b986736e207
Davoleo <davoleo2001@gmail.com>
2021-05-03 11:26:06 +0200

    Insertion Sort

--- c/theory/array1.c
+++ c/theory/array1.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 static const int VALUE_NOT_FOUND = -1;
 
@@ -46,6 +47,33 @@ int dichotomic_search(int key, int array[], int n) {
         return high;
 
     return VALUE_NOT_FOUND;
+}
+
+bool insertion_sort(int array[], int n) {
+    if (array == NULL)
+        return false;
+    
+    if (n < 1)
+        return false;
+
+    if (n == 1)
+        return true;
+
+    for (int i = 0; i < n; i++) {
+        int j;
+        int elem = array[i];
+
+        //Ricerca in tutti gli indici j positivi prima di i, e se l'elemento in j è maggiore della chiave lo si sposta in avanti
+        //per fare spazio per elem nel posto giusto
+        for (j = i - 1; j >= 0 && array[j] > elem; --j)
+            array[j + 1] = array[j];
+
+        //Elem va a finire nel posto disponibile che si è venuto a creare
+        if (j != i - 1)
+            array[j + 1] = elem;
+    }
+
+    return true;
 }
 
 int main() {
