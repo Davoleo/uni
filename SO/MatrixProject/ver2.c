8de86bace613e2fcbab39e2727df88a01e7e8ad5
Zambo-dev <thomasz01@outlook.it>
2022-01-07 15:41:32 +0100

    Fuck u

--- SO/MatrixProject/ver2.c
+++ SO/MatrixProject/ver2.c
@@ -12,10 +12,10 @@ static int matrix_length = 0;
 /**
  * \param pipe The pipe through which column values are sent and received
  * \param row The row of the first matrix this process is working on
- * //\param row_i The index of the first element of the row the process is working on
+ * \param row_id Index of the process
  * //\param result_row The row of the resulting matrix
  */
-void compute_row(int pipe[2], int parent_pipe[2], long row[matrix_length]) {
+void compute_row(int pipe[2], int parent_pipe[2], long row[matrix_length], int row_id) {
 
 	///Storage array for column values read from the pipe
 	long col_vals[matrix_length];
@@ -24,7 +24,7 @@ void compute_row(int pipe[2], int parent_pipe[2], long row[matrix_length]) {
 	long res_row[matrix_length];
 	memset(res_row, 0, matrix_length * sizeof(long));
 
-	printf("Process %d begins computing row\n", getpid());
+	printf("Process %d begins computing row\n", row_id);
 
 	//For each row value we read 3 column values from the second matrix (sent via pipe) 
 	for(int i=0; i < matrix_length; i++) {
@@ -33,21 +33,23 @@ void compute_row(int pipe[2], int parent_pipe[2], long row[matrix_length]) {
 			//Wait until 3 values have been read from the pipe
 		}
 		
-		printf("column values: %d %ld %ld %ld %ld\n", getpid(), col_vals[0], col_vals[1], col_vals[2], col_vals[3]);
+		printf("column values: %d %ld %ld %ld\n", row_id, col_vals[0], col_vals[1], col_vals[2]);
 
 		// Multiply RowCell from Matrix 1 and ColumnCell from Matrix 2 -> increment results in res_row
 		for(int j = 0; j < matrix_length; j++)
 		{
 			int idx = ((matrix_length - i) + j) % matrix_length;
-			res_row[j] += row[j] * col_vals[idx];
+			res_row[j] += row[idx] * col_vals[idx];
 		}
 
-		printf("Pid %d %ld %ld %ld %ld\n", getpid(), res_row[0], res_row[1], res_row[2], res_row[3]);
+		printf("Pid %d %ld %ld %ld\n", row_id, res_row[0], res_row[1], res_row[2]);
 		write(pipe[1], col_vals, matrix_length * sizeof(long));
 	}
 	
-	printf("Result %d -> %ld %ld %ld %ld\n", getpid(), res_row[0], res_row[1], res_row[2], res_row[3]);
+	write(parent_pipe[1], &row_id, sizeof(int));
 	write(parent_pipe[1], res_row, matrix_length * sizeof(long));
+
+	printf("Result %d -> %ld %ld %ld\n", row_id, res_row[0], res_row[1], res_row[2]);
 
 }
 
@@ -117,7 +119,7 @@ int main() {
 			for(int j=0; j < matrix_length; j++)
 				initial_values[j] = matrix1[j][i];
 			
-			compute_row(pipe_arr[i], parent_pipe, initial_values);
+			compute_row(pipe_arr[i], parent_pipe, initial_values, i);
 
 			printf("end of child %d ----------------------------------------\n", i);
 			
@@ -128,24 +130,44 @@ int main() {
 		}
 	}
 
+	for(int i=0; i < matrix_length; i++)
+		wait(NULL);
+
+	puts("end of wait");
+
 	long result_matrix[matrix_length][matrix_length];
 	long res_row[matrix_length];
+	int row_id;
 
 	for(int x=0; x < matrix_length; x++) {
+		while(read(parent_pipe[0], &row_id, sizeof(int)) <= 0) {}
 		while(read(parent_pipe[0], res_row, matrix_length * sizeof(long)) <= 0) {}
-
+		
 		for(int y=0; y < matrix_length; y++) {
-			result_matrix[x][y] = res_row[y];
+			int shift_val = (y + row_id) % matrix_length;
+			result_matrix[x][y] = res_row[shift_val];
 		}
 	}
-
 
 	for(int i=0; i < matrix_length; i++) {
 		close(pipe_arr[i][0]);
 		close(pipe_arr[i][1]);
 	}
 
-	print_matrix(result_matrix, matrix_length, matrix_length);
+	printf("\nResult --------------------\n");
+
+	for(int i =0; i < matrix_length; i++)
+		printf("%ld ", result_matrix[0][i]);
+	puts("");
+	for(int i =0; i < matrix_length; i++)
+		printf("%ld ", result_matrix[1][i]);
+	puts("");
+	for(int i =0; i < matrix_length; i++)
+		printf("%ld ", result_matrix[2][i]);
+
+	
+
+	//print_matrix(result_matrix, matrix_length, matrix_length);
 
 
 	puts("PADRE KEK"); 
