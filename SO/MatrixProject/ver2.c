22c05e2330dd43db721604a27b16ff2f915c8571
Davoleo <dav@davoleo.net>
2022-01-16 10:00:36 +0100

    get result values & fix some compiler warnings

--- SO/MatrixProject/ver2.c
+++ SO/MatrixProject/ver2.c
@@ -22,28 +22,34 @@ void compute_cell(int x, int y, int horizontal_pipe[2], int vertical_pipe[2], in
 	//For each row value we read 3 column values from the second matrix (sent via pipe) 
 	for(int i=0; i < matrix_length; i++) {
 
-		long hvalue;
-		long vvalue;
+		long hvalue = 0;
+		long vvalue = 0;
 
-		while(read(horizontal_pipe[0], hvalue, sizeof(long)) <= 0) {
+		while(read(horizontal_pipe[0], &hvalue, sizeof(long)) <= 0) {
 			//Wait until we read the input matrix value that scrolls horizontally 
 		}
-		while(read(vertical_pipe[0], vvalue, sizeof(long)) <= 0) {
+		while(read(vertical_pipe[0], &vvalue, sizeof(long)) <= 0) {
 			//Wait until we read the input matrix value that scrolls vertically
 		}
+
+		puts("Before compute_cell");
 
 		//Multiplication and add to the value buffer
 		cell_result += (hvalue * vvalue);
 
 		//Pass values to the next process through the pipes
-		write(horizontal_pipe[1], hvalue, sizeof(long));
-		write(vertical_pipe[1], vvalue, sizeof(long));
+		int hwstatus = write(horizontal_pipe[1], &hvalue, sizeof(long));
+		int vwstatus = write(vertical_pipe[1], &vvalue, sizeof(long));
+		if (hwstatus <= 0 || vwstatus <= 0) 
+			error("Error while passing values to the next process in pipes");
 	}
 
 	//Once calculations are complete pass the result to the parent through the parent_pipe
 	//Coordinates of the cell the result belongs to are also passed
 	cell_struct result_obj = { x, y, cell_result };
-	write(parent_pipe[1], &result_obj, sizeof(cell_struct));
+	if (write(parent_pipe[1], &result_obj, sizeof(cell_struct)) <= 0) {
+		error("Error while passing values to parent process.");
+	}
 }
 
 int main() {
@@ -63,19 +69,24 @@ int main() {
 	//print_matrix(matrix2, matrix_length, matrix_length);
 	
 	int parent_pipe[2];
-	pipe(parent_pipe);
+	if (pipe(parent_pipe) == -1) 
+		error("Error while creating parent pipe");
 
 	// Allocate and initialize pipes
 	int hori_pipes[matrix_length][matrix_length][2];
 	int vert_pipes[matrix_length][matrix_length][2];
 	for(int i=0; i < matrix_length; i++) {
 		for (int j=0; j < matrix_length; j++) {
-			pipe(hori_pipes[i][j]);
-			pipe(vert_pipes[i][j]);
+			int pipecreate = 0;
+			pipecreate += pipe(hori_pipes[i][j]);
+			pipecreate += pipe(vert_pipes[i][j]);
+
+			if (pipecreate < 0) 
+				error("Error while creating pipe matrices");
 		}
 	}
 
-
+	puts("----- Matrix Multiplication Begins NOW -----");
 	clock_t bench_begin = clock();
 
 	// Process id array
@@ -101,16 +112,19 @@ int main() {
 				int nexthpipe = j == 0 ? matrix_length : j - 1;
 				dup2(hori_pipes[i][nexthpipe][1], hori_pipes[i][j][1]);
 				//pass initial values into the pipe
-				write(hori_pipes[i][j][1], initial_rowvalue, sizeof(long));
+				if (write(hori_pipes[i][j][1], &initial_rowvalue, sizeof(long)) <= 0) {
+					error("Error while writing initial rowvalues as first values for the pipes");
+				}
 
 				//Link vertical rotation pipes
 				int nextvpipe = i == 0 ? matrix_length : i - 1;
 				dup2(vert_pipes[nextvpipe][j][1], vert_pipes[i][j][1]);
 				//pass initial values into the pipe
-				write(vert_pipes[i][j][1], initial_colvalue, sizeof(long));
+				if (write(vert_pipes[i][j][1], &initial_colvalue, sizeof(long)) <= 0) {
+					error("Error while writing initial colvalues as first values for the pipes");
+				}
 
-
-				compute_cell(i, j, hori_pipes[i], vert_pipes[i], parent_pipe);
+				compute_cell(i, j, hori_pipes[i][j], vert_pipes[i][j], parent_pipe);
 
 			}
 
@@ -128,7 +142,15 @@ int main() {
 	printf("Execution time: %lf milliseconds\n", (double)(bench_end - bench_begin) * 1000 / CLOCKS_PER_SEC);
 
 	long result_matrix[matrix_length][matrix_length];
-	cell_struct result;
+
+	for (int i=0; i < matrix_length * matrix_length; i++) {
+		cell_struct result;
+		while (read(parent_pipe[0], &result, sizeof(cell_struct)) <= 0) {
+			//Keep trying to read result until we actually read more than 0 bytes
+		}
+		result_matrix[result.x][result.y] = result.value;
+	}
+
 
 	//CLOSE EVERY FILE DESCRIPTOR
 	for (int i=0; i < matrix_length; i++) {
