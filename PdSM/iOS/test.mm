931d8d0ed0c3d7db76e96105c4f6940df61bbc06
Davoleo <dav@davoleo.net>
2022-03-05 13:25:18 +0100

    Activation Record Example

--- PdSM/iOS/test.mm
+++ PdSM/iOS/test.mm
@@ -1,1 +1,27 @@
 // This file can contain C++ code as well as Objective-C
+@interface Test : NSObject {
+}
+
+//Ogni volta che viene invocato un metodo: nello stack vieen creato un activation record
+//Nell'activation record in questo caso ci sarà: n, j
+- (void) countdown:(int)n {
+	int j;
+	if ([self isEven:n] == YES)
+		j = n/2;
+	else
+		j = n+1;
+	for (j; j>0; j--) {
+		//Stampa su console una stringa Obj-C anche formattata con placeholder
+		NSLog(@"%d", j);
+	}
+}
+
+//Nell'activation record ci sarà: m, even
+//l'activation record verrà distrutto non appena si ritorna fuori dal metodo
+- (BOOL) isEven:(int)num {
+	BOOL even = (num % 2 == 0);
+	if (even == YES)
+		return YES;
+	else
+		return NO;
+}
