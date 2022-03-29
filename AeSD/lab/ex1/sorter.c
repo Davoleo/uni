246b820264bbfecdfb7c2a5a10377ee83f2ad42c
davoleo <dav@davoleo.net>
2022-03-29 14:28:54 +0200

    Refactor + algoritm flag | maybe fixed another segfault

--- AeSD/lab/ex1/sorter.c
+++ AeSD/lab/ex1/sorter.c
@@ -1,7 +1,7 @@
+#include <assert.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <assert.h>
 #include <string.h>
 
 //#include "../bench.h"
@@ -10,18 +10,20 @@
 //#include "../bench.h"
 
 /**
- * Windows: .\sort.exe -m=743 -t=107
- * UNIX: ./sort -m=743 -t=107
+ * Windows: .\sort.exe -a=<int>
+ * UNIX: ./sort -a=<int>
+ *
+ * algorithm values:
+ * 0: composite algorithm [insertion|counting|shellsort]
+ * 1: shell sort on the whole array
  **/
+int algorithm = 0;
 
-int ct_swap = 0;
-int ct_cmp = 0;
 int ct_read = 0;
 
 int max_dim = 0;
 int ntests = 107;
 int details = 0;
-int graph = 0;
 
 int n = 0; /// dimensione dell'array
 
@@ -38,8 +40,6 @@ void swap(int* a, int* b)
     int tmp = *a;
     *a = *b;
     *b = tmp;
-    /// aggiorno contatore globale di swap
-    ct_swap++;
 }
 
 int partition(int* A, int p, int r)
@@ -51,7 +51,6 @@ int partition(int* A, int p, int r)
     int i = p - 1;
 
     for (int j = p; j < r; j++) {
-        ct_cmp++;
         ct_read++;
         if (A[j] <= x) {
             i++;
@@ -79,108 +78,98 @@ void quick_sort(int* A, int p, int r)
 
 void shellsort(int arr[], int num)
 {
-	for (int i = num/2; i > 0; i = i/2)
-	{
-		for (int j = i; j < num; ++j)
-		{
-			for(int k = j - i; k >= 0; k = k - i)
-			{
-				++ct_read;
-				++ct_read;
-				if (arr[k+i] >= arr[k])
-					break;
-				else
-				{
-					++ct_read;
-					++ct_read;
-					swap(arr + k, arr + k + i);
-				}
-			}
-		}
-	} 
+    for (int i = num / 2; i > 0; i = i / 2) {
+        for (int j = i; j < num; ++j) {
+            for (int k = j - i; k >= 0; k = k - i) {
+                ++ct_read;
+                ++ct_read;
+                if (arr[k + i] >= arr[k])
+                    break;
+                else {
+                    ++ct_read;
+                    ++ct_read;
+                    swap(arr + k, arr + k + i);
+                }
+            }
+        }
+    }
 }
 
 /**
  * \brief sorts arr using an insertion algorithm approach
  */
-void insertion_sort(int* arr, int length) {
-	for (int i=1; i < length; ++i) {
-		for (int j=i-1; j>=0; --j) {
-			ct_read += 2;
-			if (arr[j]>arr[j+1]) {
-				ct_read += 2;
-				swap(arr + j, arr + j + 1);
-			} else {
-				break;
-			}
-		}
+void insertion_sort(int* arr, int length)
+{
+    for (int i = 1; i < length; ++i) {
+        for (int j = i - 1; j >= 0; --j) {
+            ct_read += 2;
+            if (arr[j] > arr[j + 1]) {
+                ct_read += 2;
+                swap(arr + j, arr + j + 1);
+            } else {
+                break;
+            }
+        }
 
-		/// prints algorithm steps
-		if (details) {
-			printf("passo %d:\n",i);
-			for (int j=0; j < length; ++j) {
-				printf("%d ",arr[j]);
-			}
-			printf("\n");
-		}
-	}
+        /// prints algorithm steps
+        if (details) {
+            printf("passo %d:\n", i);
+            for (int j = 0; j < length; ++j) {
+                printf("%d ", arr[j]);
+            }
+            printf("\n");
+        }
+    }
 }
 
-void counting_sort(int *A, int *B, int *C, int n, int k)
+void counting_sort(int* A, int* B, int* C, int n, int k)
 {
-  /// A: array in input 0..n-1
-  /// B: array in output 0..n-1
-  /// C: array per conteggi 0..k
-  /// n: elementi da ordinare
-  /// k: valore massimo contenuto in A
+    /// A: array in input 0..n-1
+    /// B: array in output 0..n-1
+    /// C: array per conteggi 0..k
+    /// n: elementi da ordinare
+    /// k: valore massimo contenuto in A
 
-  for (int i = 0; i <= k; ++i)
-  { /// reset array conteggi
-    C[i] = 0;
-    //ct_opw++;
-  }
+    for (int i = 0; i <= k; ++i) { /// reset array conteggi
+        C[i] = 0;
+        // ct_opw++;
+    }
 
-  for (int j = 0; j < n; ++j)
-  { /// conteggio istogramma
-    C[A[j]]++;
-    ct_read += 2;
-  }
+    for (int j = 0; j < n; ++j) { /// conteggio istogramma
+        C[A[j]]++;
+        ct_read += 2;
+    }
 
-  if (details)
-  {
-    printf("array conteggi\n");
-    print_array(C, k + 1);
-  }
-  for (int i = 1; i <= k; ++i)
-  { /// C[i] contiene il numero di elementi <= i
-    C[i] += C[i - 1];
-    ct_read += 2;
-  }
+    if (details) {
+        printf("array conteggi\n");
+        print_array(C, k + 1);
+    }
+    for (int i = 1; i <= k; ++i) { /// C[i] contiene il numero di elementi <= i
+        C[i] += C[i - 1];
+        ct_read += 2;
+    }
 
-  if (details)
-  {
-    printf("array con conteggi accumulati\n");
-    print_array(C, k + 1);
-  }
+    if (details) {
+        printf("array con conteggi accumulati\n");
+        print_array(C, k + 1);
+    }
 
-  for (int j = n - 1; j >= 0; --j)
-  { /// per ogni elemento originale in A ->
-    /// mi chiedo nel conteggio C quanti sono gli elementi minori o uguali:
-    /// questo corrisponde alla posizione dell'elemento in B
-    if (details)
-      printf("A[%d]=%d, C[A[%d]]=%d --> scrivo B[%d-1]=%d\n", j, A[j], j, C[A[j]], C[A[j]], A[j]);
+    for (int j = n - 1; j >= 0; --j) { /// per ogni elemento originale in A ->
+        /// mi chiedo nel conteggio C quanti sono gli elementi minori o uguali:
+        /// questo corrisponde alla posizione dell'elemento in B
+        if (details)
+            printf("A[%d]=%d, C[A[%d]]=%d --> scrivo B[%d-1]=%d\n", j, A[j], j, C[A[j]], C[A[j]], A[j]);
 
-    B[C[A[j]] - 1] = A[j];
-    ct_read += 3;
-    C[A[j]] = C[A[j]] - 1;
-    ct_read += 2;
-  }
+        B[C[A[j]] - 1] = A[j];
+        ct_read += 3;
+        C[A[j]] = C[A[j]] - 1;
+        ct_read += 2;
+    }
 
-  if (details)
-  {
-    printf("array con conteggi accumulati dopo il decremento\n");
-    print_array(C, k + 1);
-  }
+    if (details) {
+        printf("array con conteggi accumulati dopo il decremento\n");
+        print_array(C, k + 1);
+    }
 }
 
 int parse_cmd(int argc, char** argv)
@@ -195,9 +184,8 @@ int parse_cmd(int argc, char** argv)
                 ntests = atoi(argv[i] + 3);
             if (argv[i][1] == 'v')
                 details = 1;
-            if (argv[i][1] == 'g') {
-                graph = 1;
-                ntests = 1;
+            if (argv[i][1] == 'a') {
+                algorithm = atoi(argv[i] + 3);
             }
         }
     }
@@ -207,8 +195,8 @@ int parse_cmd(int argc, char** argv)
 
 int main(int argc, char** argv)
 {
-	const int LOWS_OFFSET = 1000;
-	const int NEGS_OFFSET = 2000;
+    const int LOWS_OFFSET = 1000;
+    const int NEGS_OFFSET = 2000;
 
     if (parse_cmd(argc, argv))
         return 1;
@@ -238,78 +226,74 @@ int main(int argc, char** argv)
             print_array(A, n);
         }
 
-        ct_swap = 0;
-        ct_cmp = 0;
         ct_read = 0;
 
-		int* lowvals = malloc(sizeof(int) * max_dim);
-		int lowvals_size = 0;
-		int* highvals = malloc(sizeof(int) * (max_dim));
-		int highvals_size = 0;
-		int negvals[50];
-		int negvals_size = 0;
+        if (algorithm == 0) {
+            int* lowvals = malloc(sizeof(int) * max_dim);
+            int lowvals_size = 0;
+            int* highvals = malloc(sizeof(int) * (max_dim));
+            int highvals_size = 0;
+            int negvals[50];
+            int negvals_size = 0;
 
-		for (int it = 0; it < n; ++it) {
-			++ct_read;
-			int val = A[it];
-			if (val > 131) {
-				//Put value in highvalues array and its address in the original array
-				highvals[highvals_size] = val;
-				//A[it] = highvals_size;
-				++highvals_size;
-			}
-			else if (val >= 0) {
-				//Put value in lowvalues array and its address in the original array offset by 1000
-				lowvals[lowvals_size] = val;
-				//A[it] = LOWS_OFFSET + lowvals_size;
-				++lowvals_size;
-			}
-			else {
-				negvals[negvals_size] = val;
-				//A[it] = NEGS_OFFSET + negvals_size;
-				++negvals_size;
-				assert(negvals_size <= 50);
-			}
-		}
+            for (int it = 0; it < n; ++it) {
+                ++ct_read;
+                int val = A[it];
+                if (val > 131) {
+                    // Put value in highvalues array and its address in the original array
+                    highvals[highvals_size] = val;
+                    // A[it] = highvals_size;
+                    ++highvals_size;
+                } else if (val >= 0) {
+                    // Put value in lowvalues array and its address in the original array offset by 1000
+                    lowvals[lowvals_size] = val;
+                    // A[it] = LOWS_OFFSET + lowvals_size;
+                    ++lowvals_size;
+                } else {
+                    negvals[negvals_size] = val;
+                    // A[it] = NEGS_OFFSET + negvals_size;
+                    ++negvals_size;
+                    assert(negvals_size <= 50);
+                }
+            }
 
-        /// algoritmo di sorting
-        //quick_sort(A, 0, n - 1);
-		//insertion_sort(A, n);
-		
-		assert(highvals_size > 0);
-		assert(lowvals_size > 0);
+            assert(highvals_size > 0);
+            assert(lowvals_size > 0);
 
-		int increments[132];
-		int* lowvals_copy = malloc(max_dim * sizeof(int));
-		memcpy(lowvals_copy, lowvals, sizeof(int));
-		ct_read += lowvals_size;
-		//Create increments array and lowvals copy to be used as input in counting_sort
+            int increments[132];
+            int* lowvals_copy = malloc(max_dim * sizeof(int));
+            memcpy(lowvals_copy, lowvals, sizeof(int));
+            ct_read += lowvals_size;
+            // Create increments array and lowvals copy to be used as input in counting_sort
 
-		counting_sort(lowvals_copy, lowvals, increments, lowvals_size, 132);
-		free(lowvals_copy);
-		puts("Sono arrivato qui quante volte?");
+            counting_sort(lowvals_copy, lowvals, increments, lowvals_size, 132);
+            free(lowvals_copy);
 
-		shellsort(highvals, highvals_size);
-		insertion_sort(negvals, negvals_size);
+            shellsort(highvals, highvals_size);
+            insertion_sort(negvals, negvals_size);
 
-		int re_size = 0;
-		int iter = 0;
-		while (iter < negvals_size) {
-			++ct_read;
-			A[re_size++] = negvals[iter];
-			++iter;
-		}
-		iter = 0;
-		while(iter < lowvals_size) {
-			++ct_read;
-			A[re_size++] = lowvals[iter];
-			++iter;
-		}
-		while(iter < highvals_size) {
-			++ct_read;
-			A[re_size++] = highvals[iter];
-			++iter;
-		}
+            int re_size = 0;
+            int iter = 0;
+            while (iter < negvals_size) {
+                ++ct_read;
+                A[re_size++] = negvals[iter];
+                ++iter;
+            }
+            iter = 0;
+            while (iter < lowvals_size) {
+                ++ct_read;
+                A[re_size++] = lowvals[iter];
+                ++iter;
+            }
+			iter = 0;
+            while (iter < highvals_size) {
+                ++ct_read;
+                A[re_size++] = highvals[iter];
+                ++iter;
+            }
+        } else if (algorithm == 1) {
+            shellsort(A, n);
+        }
 
         if (details) {
             printf("Output:\n");
@@ -324,8 +308,8 @@ int main(int argc, char** argv)
             read_max = ct_read;
         printf("Test %d %d\n", test, ct_read);
 
-		free(highvals);
-		free(lowvals);
+        // free(highvals);
+        // free(lowvals);
     }
 
     printf("%d,%d,%.1f,%d\n",
