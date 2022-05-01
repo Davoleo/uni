af83ce9f79d0644d80ecae644bc0d49ce23f9c8f
Davoleo <dav@davoleo.net>
2022-05-01 11:11:19 +0200

    Ex 4 - Scheda algebra

--- CN/lab_28_4_2022/algebra.m
+++ CN/lab_28_4_2022/algebra.m
@@ -3,6 +3,7 @@
 size = 10;
 symmtridiag(size, rand(1, size) * 10, rand(1, size-1) * 10);
 
+pause
 
 %% Ex. 2
 clear
@@ -30,3 +31,49 @@ y = reverse_substitution(A, b)';
 err = norm(y - yexact)
 
 condi = cond(A)
+
+pause
+
+%% Ex. 4
+clc
+clear
+
+A = [ 2 3 1;
+	  1 -1 1;
+	  0 2 2  ];
+B = [ 1 2 1;
+	 -1 2 -1;
+	  2 0 2 ];
+C = [ 1 0 1;
+	  0 1 1;
+	  1 0 2 ];
+
+% Per valutare Se sono diverse si può anche fare la norma della differenza tra le 2 matrici risultanti
+% > Se è zero -> Le 2 matrici sono uguali
+% Rimuovere ';' per confrontare i 2 risultati
+AB = A*B;
+BA = B*A;
+if norm(AB - BA) ~= 0
+	disp('AB e BA sono diverse');
+end
+
+% Rimuovere ';' per confrontare i 2 risultati
+ApBpC = (A + B) + C;
+BpCpA = A + (B + C);
+if norm(ApBpC - BpCpA) == 0
+	disp('AB e BA sono uguali');
+end
+
+% Rimuovere ';' per confrontare i 2 risultati
+BCA = A * (B * C);
+ABC = (A * B) * C;
+if norm(ABC - BCA) == 0
+	disp('A(BC) e (AB)C sono uguali');
+end
+
+% Rimuovere ';' per confrontare i 2 risultati
+ABtrasp = (A*B)';
+BtraspAtrasp = B'*A';
+if norm(ABtrasp - BtraspAtrasp) == 0
+	disp("(AB)' e B'A' sono uguali");
+end
