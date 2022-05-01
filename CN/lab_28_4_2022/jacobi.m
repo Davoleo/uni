e8f1691b965d3fc10c50fd313de7137b6629640a
Davoleo <dav@davoleo.net>
2022-05-01 11:51:15 +0200

    jacobi initial work

---
+++ CN/lab_28_4_2022/jacobi.m
@@ -0,0 +1,27 @@
+
+function X = jacobi(x0, A, b, treshold)
+%jacobi - Risolve il sistema tramite il Metodo Iterativo di Jacobi
+%
+% Syntax: result = jacobi(x0, matrix)
+%
+% Risolve il sistema Ax = b tramite Metodo Iterativo Lineare di Jacobi
+
+	% Size della matrice
+	[n, ~] = size(A);
+
+	% Passo 1
+
+	% Passo 2
+
+	% Passo 3..
+	k = 3
+
+	while abs(X(k+1, :) - X(k, :)) < treshold
+		for i = 1:n
+		
+		end
+
+		k = k + 1;
+	end	
+
+end
