5f945e15754cb4973659ba6df8bccdf50ea215a3
Davoleo <dav@davoleo.net>
2022-05-28 12:50:11 +0200

    integrazione numerica via Cavalieri-Simpson

--- CN/lab10/ex_integral.m
+++ CN/lab10/ex_integral.m
@@ -30,6 +30,8 @@ Q0 = H * sum(fvalues)
 fvalues = f(interv(1)) + f(interv(end)) + 2*sum(f(interv([2:end-1])));
 Q1 = H/2 * sum(fvalues)
 
-
+%% Formula di Cavalieri-Simpson
+fvalues = f(interv(i)) + 4*f((interv(i)+interv(i+1)) / 2) + f(i+1);
+Q2 = H/6 * sum(fvalues)
 
 matlab_ver = integral(f, a, b)
