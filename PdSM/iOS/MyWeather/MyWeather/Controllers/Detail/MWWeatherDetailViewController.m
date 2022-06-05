27e1d9b3627a5d9a25bbe0d2fef0c3d424223b8a
davoleo <dav@davoleo.net>
2022-06-05 17:47:10 +0200

    Detail Screen work

--- PdSM/iOS/MyWeather/MyWeather/Controllers/Detail/MWWeatherDetailViewController.m
+++ PdSM/iOS/MyWeather/MyWeather/Controllers/Detail/MWWeatherDetailViewController.m
@@ -7,6 +7,7 @@
 
 #import "MWWeatherDetailViewController.h"
 #import "MWUtils.h"
+#import "MWWeatherCardView.h"
 
 @interface MWWeatherDetailViewController ()
 
@@ -21,6 +22,12 @@
 
 @property (weak, nonatomic) IBOutlet UILabel* sunriseLabel;
 @property (weak, nonatomic) IBOutlet UILabel* sunsetLabel;
+
+@property (weak, nonatomic) IBOutlet UIStackView *hourlyStack;
+@property (weak, nonatomic) IBOutlet UIStackView *dailyStack;
+@property (weak, nonatomic) IBOutlet UIView *hourlyStubView;
+
+@property (nonatomic) MWTemperatureMetrics metricPreference;
 
 @end
 
@@ -53,34 +60,63 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     if (self.forecast != nil) {
-        [self setupUI];
+        [self updateCurrentWeatherUI];
+        self.metricPreference = (MWTemperatureMetrics) [[NSUserDefaults standardUserDefaults] integerForKey:MW_TEMPERATURE_METRIC_PREF];
     }
 }
 
 
 - (void) setupUI {
-    //Attach map pin image to location name & create NSString
-    /*
-    NSTextAttachment* placemarkAttachment = [[NSTextAttachment alloc] init];
-    placemarkAttachment.image = [UIImage systemImageNamed:@"mappin"];
-    NSAttributedString* placemarkAttachString = [NSAttributedString attributedStringWithAttachment:placemarkAttachment];
-    NSMutableAttributedString* placemarkString = [[NSMutableAttributedString alloc] initWithString:self.position.placemarkCache.name];
-    [placemarkString appendAttributedString:placemarkAttachString];
-    */
 
+    [self updateCurrentWeatherUI];
+
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"HH:mm"];
+    [self.forecast.hourly enumerateObjectsUsingBlock:^ (MWWeatherData* weather, NSUInteger index, BOOL* stop) {
+        [self.hourlyStack addArrangedSubview:[self cardWith:weather AndDateFormatter:formatter]];
+    }];
+
+    [formatter setDateFormat:@"E HH:mm"];
+    [self.forecast.daily enumerateObjectsUsingBlock:^ (MWWeatherData* weather, NSUInteger index, BOOL* stop) {
+        [self.dailyStack addArrangedSubview:[self cardWith:weather AndDateFormatter:formatter]];
+    }];
+}
+
+
+- (MWWeatherCardView*) cardWith: (MWWeatherData*) weatherData AndDateFormatter: (NSDateFormatter*) formatter {
+    MWWeatherCardView* card = [[MWWeatherCardView alloc] init];
+
+    //Hour/Day
+    NSDate* date = [NSDate dateWithTimeIntervalSince1970:weatherData.timestamp];
+    card.dateLabel.text = [formatter stringFromDate:date];
+
+    //Weather Icon
+    BOOL isNight = weatherData.timestamp > weatherData.sunset || weatherData.timestamp < weatherData.sunrise;
+    card.weatherIcon.image = [UIImage systemImageNamed:[weatherData.condition decodeSystemImageNameAtNight:isNight]];
+
+    //Min and Max Temperatures Label
+    card.conditionsLabel.text = weatherData.condition.name;
+    char tempChar = [MWUtils temperatureFormatCharForMetric:self.metricPreference];
+    double convertedMinTemperature = [MWUtils getTemperature:weatherData.minTemperature InMetric:self.metricPreference];
+    double convertedMaxTemperature = [MWUtils getTemperature:weatherData.maxTemperature InMetric:self.metricPreference];
+    NSString* temperatureString = [NSString stringWithFormat:@"Min: %.1lf°%c\nMax: %.1lf°%c", convertedMinTemperature, tempChar, convertedMaxTemperature, tempChar];
+    NSArray<NSString*>* temperatureComponents = [temperatureString componentsSeparatedByString:@"\n"];
+    NSMutableAttributedString* temperatureAttrString = [[NSMutableAttributedString alloc] initWithString:temperatureString];
+    [temperatureAttrString addAttribute:NSForegroundColorAttributeName value:[UIColor systemBlueColor] range:[temperatureString rangeOfString:temperatureComponents.firstObject]];
+    [temperatureAttrString addAttribute:NSForegroundColorAttributeName value:[UIColor systemRedColor] range:[temperatureString rangeOfString:temperatureComponents.lastObject]];
+    card.temperatureLabel.attributedText = temperatureAttrString;
+
+    return card;
+}
+
+- (void) updateCurrentWeatherUI {
     MWWeatherData* currentWeather = self.forecast.current;
 
     self.placemarkLabel.text = self.position.placemarkCache.name;
 
     //Current Temperature formatting
     MWTemperatureMetrics tempMetric = (MWTemperatureMetrics) [[NSUserDefaults standardUserDefaults] integerForKey:MW_TEMPERATURE_METRIC_PREF];
-    double temperature = currentWeather.temperature;
-    if (tempMetric == MWTemperatureMetricCelsius)
-        temperature -= 273.15;
-    else if (tempMetric == MWTemperatureMetricFahrenheit) {
-        temperature -= 273.15;
-        temperature = (temperature * 9/5) + 32;
-    }
+    double temperature = [MWUtils getTemperature:currentWeather.temperature InMetric:tempMetric];
 
     self.temperatureLabel.text = [NSString stringWithFormat:@"%.1lf°%c", temperature, [MWUtils temperatureFormatCharForMetric:tempMetric]];
 
@@ -92,15 +128,13 @@
 
     //Wind direction icon
     self.windIcon.image = [UIImage systemImageNamed:currentWeather.windDirection.iconName];
-    self.windLabel.text = [NSString stringWithFormat:@"Direction: %@ | Speed: %.2lfm/s", currentWeather.windDirection.direction, currentWeather.windSpeed];
+    self.windLabel.text = [NSString stringWithFormat:@"Direction: %@  -  Speed: %.2lfm/s", currentWeather.windDirection.direction, currentWeather.windSpeed];
 
     NSDate* currentSunrise = [NSDate dateWithTimeIntervalSince1970:currentWeather.sunrise];
     self.sunriseLabel.text = [NSString stringWithFormat:@"Sunrise: %@", [NSDateFormatter localizedStringFromDate:currentSunrise dateStyle:NSDateFormatterNoStyle timeStyle: NSDateFormatterShortStyle]];
-    NSDate* currentSunset = [NSDate dateWithTimeIntervalSince1970:currentWeather.sunrise];
+    NSDate* currentSunset = [NSDate dateWithTimeIntervalSince1970:currentWeather.sunset];
     self.sunsetLabel.text = [NSString stringWithFormat:@"Sunset: %@", [NSDateFormatter localizedStringFromDate:currentSunset dateStyle:NSDateFormatterNoStyle timeStyle: NSDateFormatterShortStyle]];
-
 }
-
 
 /*
 #pragma mark - Navigation
