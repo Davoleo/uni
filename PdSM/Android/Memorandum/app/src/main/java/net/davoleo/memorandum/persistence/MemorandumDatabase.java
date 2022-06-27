3e508a87bbfb2c79587611bf6ed3e4985b21ddcc
Davoleo <dav@davoleo.net>
2022-06-27 12:32:21 +0200

    TypeConverters & disable exportSchema

--- PdSM/Android/Memorandum/app/src/main/java/net/davoleo/memorandum/persistence/MemorandumDatabase.java
+++ PdSM/Android/Memorandum/app/src/main/java/net/davoleo/memorandum/persistence/MemorandumDatabase.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import net.davoleo.memorandum.model.Memo;
 
-@Database(entities = {Memo.class}, version = 1)
+@Database(entities = {Memo.class}, version = 1, exportSchema = false)
+@TypeConverters({net.davoleo.memorandum.persistence.TypeConverters.class})
 public abstract class MemorandumDatabase extends RoomDatabase {
 
     public static MemorandumDatabase instance;
