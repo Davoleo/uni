eb1256e8053213f1f70dddc362956f84cfb697d8
Davoleo <dav@davoleo.net>
2022-11-10 12:23:50 +0100

    Example01 [Concurrency, Threads, Locking, Java8 Features]

---
+++ IdS/src/net/davoleo/uni/examples/Example01.java
@@ -0,0 +1,62 @@
+package net.davoleo.unipr.examples;
+
+public class Example01 {
+
+	private Object mutex = new Object();
+
+	private boolean waitInProgress = false;
+
+	public void go() {
+		waitInProgress = false;
+
+		Thread notifier = new Thread(this::doNotify);
+
+		Thread waiter = new Thread(this::doWait);
+
+		notifier.start();
+		waiter.start();
+	}
+
+	private void doWait() {
+		System.out.println("Waiter started");
+
+		synchronized(mutex) {
+			waitInProgress = true;
+			mutex.notifyAll();
+
+			try {
+				mutex.wait();
+			} catch (InterruptedException e) {
+				// Il thread è stato terminato dall'esterno -> Nessun problema in questo caso
+			}
+		}
+
+		waitInProgress = false;
+
+		System.out.println("Waiter terminated");
+	}
+
+	private void doNotify() {
+		System.out.println("Notifier started");
+
+		synchronized(mutex) {
+			try {
+				while (!waitInProgress)
+					mutex.wait();
+
+				Thread.sleep(5000);
+
+				mutex.notifyAll();
+			} catch (Throwable throwable) {
+				//noop
+			}
+		}
+
+		System.out.println("Notifier terminated");
+	}
+
+	public static main void(String[] args) {
+		new Example01().go();
+	}
+
+}
