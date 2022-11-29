44b9d4041a8635d354af7e720b1309643d0a22da
Davoleo <dav@davoleo.net>
2022-11-29 09:12:42 +0100

    Resource content

--- IdS/Example05/src/net/davoleo/uni/examples/ResourceContent.java
+++ IdS/Example05/src/net/davoleo/uni/examples/ResourceContent.java
@@ -3,4 +3,26 @@ package net.davoleo.uni.examples;
 public class ResourceContent {
 	
 	private String url;
+	
+	private byte[] data;
+
+	public ResourceContent(String url, byte[] data) {
+		if (url == null || url.length() == 0)
+			throw new IllegalArgumentException("url == null || url.length() == 0");
+
+		if (data == null)
+			throw new IllegalArgumentException("data == null");
+
+		this.url = url;
+
+		this.data = data;
+	}
+
+	public String getURL() {
+		return url;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
 }
