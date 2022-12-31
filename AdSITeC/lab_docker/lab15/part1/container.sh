defea5fded57b9bca0f864ca3eb6c06dd8f4bb5a
Davoleo <dav@davoleo.net>
2022-12-31 10:49:29 +0100

    Docker lab 15.1-2 fixes

--- AdSITeC/lab_docker/lab15/part1/container.sh
+++ AdSITeC/lab_docker/lab15/part1/container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -dP --name fedora_srv -v /mnt/d/Coding/lang-projects/Universita/AdSITeC/lab_docker/lab15/part1/html/index.html:/srv/www/http/index.html fedora_srv
+docker run -dP --rm --name fedora_srv -v $PWD/html/index.html:/srv/www/http/index.html fedora_srv
