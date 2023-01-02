36f93e0db2ce6312e4ae8868995dfb0f594d674d
Davoleo <dav@davoleo.net>
2023-01-02 11:39:45 +0100

    Dynamic path

--- RdC/7-DockerNetworking/nginx-net/nginx-net-setup.sh
+++ RdC/7-DockerNetworking/nginx-net/nginx-net-setup.sh
@@ -5,7 +5,7 @@ docker build -t nginx-net .
 
 for i in {2..4}; do
 	# Start container with extended privileges custom name, custom mapped port and a bound volume to the web folder here based on nginx-net
-	docker run --privileged -d --name "nginx$i" -p "800$i:80" -v /mnt/d/Coding/lang-projects/Universita/RdC/7-DockerNetworking/web:/usr/share/nginx/html nginx-net
+	docker run --privileged -d --name "nginx$i" -p "800$i:80" -v "$PWD/../web:/usr/share/nginx/html" nginx-net
 	# Start SSH daemon on each container
 	docker exec -it nginx$i service ssh start
 done
