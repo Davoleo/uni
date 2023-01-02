7dbc1401311b858634fea2d1f4d07779c64af9ae
Davoleo <dav@davoleo.net>
2023-01-02 09:53:41 +0100

    7-DockerNetworking [part 1]

---
+++ RdC/7-DockerNetworking/nginx-net/nginx-net-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build docker image named nginx-net based Dockerfile in this folder
+docker build -t nginx-net .
+
+for i in {2..4}; do
+	# Start container with extended privileges custom name, custom mapped port and a bound volume to the web folder here based on nginx-net
+	docker run --privileged -d --name "nginx$i" -p "800$i:80" -v /mnt/d/Coding/lang-projects/Universita/RdC/7-DockerNetworking/web:/usr/share/nginx/html nginx-net
+	# Start SSH daemon on each container
+	docker exec -it nginx$i service ssh start
+done
