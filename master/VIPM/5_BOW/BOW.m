9fd4618c7b6c7f58443ff439c9db6f5e6b8d2a51
davoleo <dav@davoleo.net>
2025-11-20 16:58:24 +0100

    VIPM: BOW improvements attempts

--- master/VIPM/5_BOW/BOW.m
+++ master/VIPM/5_BOW/BOW.m
@@ -1,8 +1,11 @@
+clc
+clear
+
 disp("creazione griglia")
 
 pointPositions = [];
 featStep = 30; % TODO TBD
-imsize = 500; % TODO TBD
+imsize = 500; % Le immagini sono sempre 256x384 o 384x256
 
 tic
 for ii = featStep:featStep:imsize-featStep
@@ -16,7 +19,7 @@ toc
 disp('estrazione features')
 
 % TODO: TBD, fix nel caso di necessità del validation set
-Nim4training = 70;
+Nim4training = 80;
 
 features = [];
 labels = [];
@@ -24,15 +27,30 @@ tic
 for class=0:9
 	for nimage=0:Nim4training-1
 		im=im2double(imread(['image.orig/' num2str(100*class+nimage), '.jpg']));
-		im=imresize(im, [imsize imsize]); % TBD provare a fare crop invece che resize (stretch)
+		
+		% croppo l'immagine con dimensioni pari al minimo tra le due, in modo che la dimensione maggiore sia tagliata mantenendo il contenuto centrale
+		%padding = floor((size(im)-imsize)/2);
+		%im=im(padding(1)+1:imsize, padding(2)+1:imsize, :);
+		
+		%im=imresize(im, [imsize imsize]);
+
+		% Resize while keeping ratio
+		realsize = size(im, 1:2);
+		scale = min(imsize/realsize(2), imsize/realsize(1));
+		im = imresize(im, scale);
 		im=rgb2gray(im);
+
+		realsize = size(im, 1:2);
+		impadded = zeros(imsize);
+		impadded(imsize/2+(1:realsize(1))-floor(realsize(1)/2), ...
+		imsize/2+(1:realsize(2))-floor(realsize(2)/2))=im;
+
 		% specifichiamo il metodo perché di default non sa inferirlo
 		% il sottoinsieme di punti ridotto di quelli sul bordo non mi interessa per come sono stati calcolati i punti quindi dontcare
-		[imfeatures,dontcare] = extractFeatures(im, pointPositions, 'Method', 'SURF');
+		[imfeatures,dontcare] = extractFeatures(impadded, pointPositions, 'Method', 'SURF');
 		features = [features; imfeatures];
 		% per ogni riga (descrittore) costruiamo una colonna con le categorie e il numero dell'immagine
 		labels = [labels; repmat(class,size(imfeatures, 1), 1) repmat(nimage, size(imfeatures, 1), 1)];
-
 	end
 end
 toc
@@ -40,10 +58,10 @@ toc
 %% creazione del vocabolario
 disp('kmeans')
 % 100 parole
-K = 100; %TODO - TBD
+K = 1000; %TODO - TBD
 tic
 % Clusterizzare le 100k feature in K cluster
-[IDX, C] = kmeans(features, K);
+[IDX, C] = kmeans(features, K, "Replicates", 3);
 toc
 
 %% istogrammi BOW training
@@ -57,10 +75,11 @@ for class=0:9
 		% posizioni u in cui la prima colonna corrisponde alla classe che stiamo valutando 
 		% e la seconda colonna corrisponde all'immagine che stiamo valutando
 		u=find(labels(:,1) == class & labels(:, 2) == nimage);
-		% rappresentazione con istogramma delle feature in posizione u
+		% features in posizione u (immagine con relativa categorizzazione [label])
 		imfeaturesIDX = IDX(u);
+		% rappresentazione con istogramma della frequenza delle feature all'interno di questo elem
 		% hist: Decide il passo dell'istogramma da solo e sceglie se tagliare inizio / fine, 
-		% tramite il secondo parametro forziamo la rappresentazione di tutti e K i valori 
+		% 	tramite il secondo parametro forziamo la rappresentazione di tutti e K i valori 
 		H = hist(imfeaturesIDX, 1:K);
 		% normalizziamo l'istogramma in modo che la somma sia 1
 		% -> possiamo confrontare anche immagini con grandezze diverse 
@@ -72,8 +91,14 @@ end
 toc
 
 %% classificatore
+disp('classifier')
 % input: BOW_tr e labels_tr
-% TODO
+
+knn = fitcknn(BOW_tr, labels_tr, NumNeighbors=12, Distance="euclidean");
+
+%template = templateLinear("Learner", "svm", "Regularization", "ridge");
+%SVM = fitcecoc(BOW_tr, labels_tr, "Learners", template, "Coding", "onevsall", "Verbose", 1);
+
 
 %% istogrammi BOW test
 disp('rappresentazione BOW test')
@@ -83,12 +108,27 @@ labels_te = [];
 tic
 for class=0:9
 	for nimage=Nim4training:99
-		im=im2double(imread(["image.orig/" num2str(100*class+nimage), '.jpg']));
-		im=imresize(im, [imsize imsize]); % TBD provare a fare crop invece che resize (stretch)
+		im=im2double(imread(['image.orig/' num2str(100*class+nimage), '.jpg']));
+
+		% croppo l'immagine con dimensioni pari al minimo tra le due, in modo che la dimensione maggiore sia tagliata mantenendo il contenuto centrale
+		%padding = floor((size(im)-imsize)./2);
+		%im=im(padding(1)+1:imsize, padding(2)+1:imsize, :);
+
+		%im=imresize(im, [imsize imsize]); % TBD provare a fare crop invece che resize (stretch)
+
+		realsize = size(im, 1:2);
+		scale = min(imsize/realsize(2), imsize/realsize(1));
+		im = imresize(im, scale);
 		im=rgb2gray(im);
+
+		realsize = size(im, 1:2);
+		impadded = zeros(imsize);
+		impadded(imsize/2+(1:realsize(1))-floor(realsize(1)/2), ...
+			imsize/2+(1:realsize(2))-floor(realsize(2)/2))=im;
+
 		% specifichiamo il metodo perché di default non sa inferirlo
 		% il sottoinsieme di punti ridotto di quelli sul bordo non mi interessa per come sono stati calcolati i punti quindi dontcare
-		[imfeatures,dontcare] = extractFeatures(im, pointPositions, 'Method', 'SURF');
+		[imfeatures,dontcare] = extractFeatures(impadded, pointPositions, 'Method', 'SURF');
 
 		% Distanze di ogni punto della matrice dall'altro punto dell'altra matrice.
 		% Se le parole nel vocabolario diventano molto la grandezza di d potrebbe esplodere
@@ -107,31 +147,31 @@ toc
 %% classificazione del test set
 % Applicazione di nearest neighbor per la classificazione (rubo la classe dell'elemento più vicino del training set)
 disp("classificazione del test set")
-predicted_class = [];
-tic
-for ii = 1:size(BOW_te, 1)
-	H = BOW_te(ii, :);
-	DH = pdist2(H, BOW_tr);
-	% Trovo vettore di training con la distanza minima
-	u = find(DH==min(DH));
-	% Se ne escono più di uno prendo il primo (edge case)
-	u = u(1);
-	predicted_class = [predicted_class; labels_tr(u)];
-end
-toc
+% predicted_class = [];
+% tic
+% for ii = 1:size(BOW_te, 1)
+% 	H = BOW_te(ii, :);
+% 	DH = pdist2(H, BOW_tr);
+% 	% Trovo vettore di training con la distanza minima
+% 	u = find(DH==min(DH));
+% 	% Se ne escono più di uno prendo il primo (edge case)
+% 	u = u(1);
+% 	predicted_class = [predicted_class; labels_tr(u)];
+% end
+% toc
 
-% TODO : inserire vero classificatore
+%predicted_class_svm = predict(SVM, BOW_te);
+predicted_class_knn = predict(knn, BOW_te);
 
 %% misurazione performance
 
 % Confusion Matrix
-CM = confusionmat(labels_te, predicted_class);
+CM = confusionmat(labels_te, predicted_class_knn);
 CM = CM./repmat(sum(CM, 2), 1, size(CM, 2));
 CM
 
 % Calcolo dell'accuracy
 accuracy = mean(diag(CM))
-
 
 % Assignment 5: Scadenza 2 settimane
 % Modificare i TODO in modo di arrivare nel range 75-80% di accuracy 
