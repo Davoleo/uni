566aa966ac0a873953676f727b7ef45f001f8ffd
davoleo <dav@davoleo.net>
2026-02-11 15:08:37 +0100

    pytorch baseline - based on resnet - no data augmentation - 30 mins training on RX 9070

--- master/VIPM/final_project/baseline.py
+++ master/VIPM/final_project/baseline.py
@@ -1,93 +1,185 @@
-# Disable CPU optimization warnings (we run on GPU)
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
 
-import keras
-import keras.layers as layers
-import tensorflow as tf
+import torchvision
+from torchvision import datasets, models, transforms
+
+import numpy as np
 import matplotlib.pyplot as plt
 
-size2D = (224, 224)
-size3D = (224, 224, 3)
+import os
+import time
+from tempfile import TemporaryDirectory
+
+cudnn.benchmark = True
+plt.ion()
+
+data_transforms = {
+	'train': transforms.ToTensor(),
+	'val': transforms.ToTensor(),
+}
+
+# Data Directories
+train_dir = 'data/train'
+val_dir = 'data/valid'
+# Datasets
+train_ds = datasets.ImageFolder(train_dir, data_transforms['train'])
+val_ds = datasets.ImageFolder(val_dir, data_transforms['val'])
+
+# Data Loading
+train_loader = torch.utils.data.DataLoader(train_ds, batch_size=8, shuffle=True, num_workers=4)
+val_loader = torch.utils.data.DataLoader(val_ds, batch_size=8, shuffle=True, num_workers=4)
+
+# dataset sizes
+train_size = len(train_ds)
+val_size = len(val_ds)
+
+# Classes
+class_names = train_ds.classes
+
+# Use accelerator to train
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu'
+print(f"Accelerator: {device}")
 
 
-batch_size = 64
-train_ds = keras.utils.image_dataset_from_directory('data/train', labels='inferred', label_mode='int', image_size=size2D, batch_size=batch_size)
-valid_ds = keras.utils.image_dataset_from_directory('data/valid', labels='inferred', label_mode='int', image_size=size2D, batch_size=batch_size)
-test_ds = keras.utils.image_dataset_from_directory('data/test', labels='inferred', label_mode='int', image_size=size2D, batch_size=batch_size)
+def imdisplay(input, title=None):
+	"""Display tensor as image"""
+	input = input.numpy().transpose((1,2,0))
+	input = np.clip(input, 0, 1)
+	plt.imshow(input)
+	if title is not None:
+		plt.title(title)
+	plt.pause(0.001)	# Pause allows to display current pyplot
 
-class_names = train_ds.class_names
+def plotflush():
+	plt.ioff()
+	plt.show()	
 
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-    
+# Load a batch
+inputs, classes = next(iter(train_loader))
 
+grid = torchvision.utils.make_grid(inputs)
+imdisplay(grid, title=[class_names[x] for x in classes])
 
-# Prepare for performance
-AUTOTUNE = tf.data.AUTOTUNE
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-valid_ds = valid_ds.cache().prefetch(buffer_size=AUTOTUNE)
+def train(model, lossfun, optimizer, scheduler, num_epochs=20):
+	since = time.time()
 
+	with TemporaryDirectory() as tempdir:
+		best_model_path = os.path.join(tempdir, 'best_model_params.pt')
+		torch.save(model.state_dict(), best_model_path)
 
-baseline = keras.models.Sequential()
-baseline.add(keras.Input(shape=size3D))
+		max_accuracy = 0.0
 
-baseline.add(layers.Rescaling(1./255))
+		for epoch in range(num_epochs):
+			print(f'Epoch progress: {epoch}/{num_epochs - 1}')
+			print('-' * 20)
 
-baseline.add(layers.Conv2D(64, kernel_size=3, activation='relu'))
-baseline.add(layers.MaxPool2D(pool_size=2))
+			# Phases
+			for phase in ['train', 'val']:
+				dataloader = None
+				# Change model mode depending on phase
+				if phase == 'train':
+					model.train()
+					dataloader = train_loader
+				else:
+					model.eval()
+					dataloader = val_loader
+				
+				running_loss = 0.0
+				running_corrects = 0
 
-baseline.add(layers.Conv2D(128, kernel_size=3, activation='relu')),
-baseline.add(layers.MaxPool2D(pool_size=4))
+				# Iterate
+				for inputs,labels in dataloader:
+					# Send to gpu
+					inputs = inputs.to(device)
+					labels = labels.to(device)
 
-baseline.add(layers.Conv2D(256, kernel_size=3, activation='relu'),)
-baseline.add(layers.MaxPool2D(pool_size=4),)
+					# Reset gradient
+					optimizer.zero_grad()
 
-baseline.add(layers.Flatten(),)
-baseline.add(layers.Dropout(0.3),)
+					# forward - gradient only used in train phase
+					with torch.set_grad_enabled(phase == 'train'):
+						outputs = model(inputs)
+						_, preds = torch.max(outputs, 1)
+						loss = lossfun(outputs, labels)
 
-baseline.add(layers.Dense(100, activation='softmax'))
+						# backward + optimization in training
+						if phase == 'train':
+							loss.backward()
+							optimizer.step()
+					
+					# stats
+					running_loss += loss.item() * inputs.size(0)
+					running_corrects += torch.sum(preds == labels.data)
 
-baseline.summary()
+				if phase == 'train':
+					scheduler.step()
 
+				epoch_loss = running_loss / (train_size if phase == 'train' else val_size)
+				epoch_accuracy = running_corrects.double() / (train_size if phase == 'train' else val_size)
 
+				print(f"{phase} Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f}")
 
-# Training
-epochs = 10
+				# deep copy
+				if phase == 'val' and epoch_accuracy > max_accuracy:
+					max_accuracy = epoch_accuracy
+					torch.save(model.state_dict(), best_model_path)
+			print()
+		
+		time_elapsed = time.time() - since
+		print(f'training time: {time_elapsed // 60:.0f}mins {time_elapsed % 60:.0f}secs')
+		print(f'Best Val Accuracy: {max_accuracy:4f}')
 
-baseline.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-history = baseline.fit(train_ds, epochs=epochs, validation_data=valid_ds)
+		model.load_state_dict(torch.load(best_model_path, weights_only=True))
+	return model
 
-def plot_performance(history):
-	acc = history.history['accuracy']
-	val_acc = history.history['val_accuracy']
+def display_predicts(model, num_images=6):
+	was_training = model.training
+	model.eval()
+	image_counter = 0
+	_ = plt.figure()
 
-	loss = history.history['loss']
-	val_loss = history.history['val_loss']
+	with torch.no_grad():
+		for i, (inputs, labels) in enumerate(val_loader):
+			inputs = inputs.to(device)
+			labels = labels.to(device)
 
-	epochs_range = range(epochs)
+			outputs = model(inputs)
+			_, preds = torch.max(outputs, 1)
+			
+			for j in range(inputs.size()[0]):
+				image_counter += 1
+				ax = plt.subplot(num_images // 2, 2, image_counter)
+				ax.axis('off')
+				ax.set_title(f'predicted: {class_names[preds[j]]}')
+				plt.imshow(inputs.cpu().data[j])
 
-	plt.figure(figsize=(8, 8))
-	plt.subplot(1, 2, 1)
-	plt.plot(epochs_range, acc, label='Training Accuracy')
-	plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-	plt.legend(loc='lower right')
-	plt.title('Training and Validation Accuracy')
+				if image_counter == num_images:
+					model.train(mode=was_training)
+					return
+		model.train(mode=was_training)
 
-	plt.subplot(1, 2, 2)
-	plt.plot(epochs_range, loss, label='Training Loss')
-	plt.plot(epochs_range, val_loss, label='Validation Loss')
-	plt.legend(loc='upper right')
-	plt.title('Training and Validation Loss')
-	plt.show()
+model_base = models.resnet18(weights='IMAGENET1K_V1')
 
-plot_performance(history)
+# Fully connected layer input features
+num_features = model_base.fc.in_features
 
-score = baseline.evaluate(test_ds, verbose=0)
-print("test loss: ", score[0])
-print("test accuracy: ", score[1])
+# Replace new Fully connected layer
+model_base.fc = nn.Linear(num_features, len(class_names))
+
+model_ft = model_base.to(device)
+loss = nn.CrossEntropyLoss()
+
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Learning rate decay: 0.1 factor every 7 epochs
+ft_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+# Train and Evaluate
+model_ft = train(model_ft, loss, optimizer_ft, ft_lr_scheduler, num_epochs=20)
+display_predicts(model_ft)
+
+plotflush()
---
+++ master/VIPM/final_project/baseline_tensorflow.py
@@ -0,0 +1,93 @@
+# Disable CPU optimization warnings (we run on GPU)
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import keras
+import keras.layers as layers
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+size2D = (224, 224)
+size3D = (224, 224, 3)
+
+
+batch_size = 32
+train_ds = keras.utils.image_dataset_from_directory('data/train', labels='inferred', label_mode='int', image_size=size2D, batch_size=batch_size)
+valid_ds = keras.utils.image_dataset_from_directory('data/valid', labels='inferred', label_mode='int', image_size=size2D, batch_size=batch_size)
+test_ds = keras.utils.image_dataset_from_directory('data/test', labels='inferred', label_mode='int', image_size=size2D, batch_size=batch_size)
+
+class_names = train_ds.class_names
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+    
+
+
+# Prepare for performance
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+valid_ds = valid_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+baseline = keras.models.Sequential()
+baseline.add(keras.Input(shape=size3D))
+
+baseline.add(layers.Rescaling(1./255))
+
+baseline.add(layers.Conv2D(64, kernel_size=3, activation='relu'))
+baseline.add(layers.MaxPool2D(pool_size=2))
+
+baseline.add(layers.Conv2D(128, kernel_size=3, activation='relu')),
+baseline.add(layers.MaxPool2D(pool_size=4))
+
+baseline.add(layers.Conv2D(256, kernel_size=3, activation='relu'),)
+baseline.add(layers.MaxPool2D(pool_size=4),)
+
+baseline.add(layers.Flatten(),)
+baseline.add(layers.Dropout(0.3),)
+
+baseline.add(layers.Dense(100, activation='softmax'))
+
+baseline.summary()
+
+
+
+# Training
+epochs = 10
+
+baseline.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = baseline.fit(train_ds, epochs=epochs, validation_data=valid_ds)
+
+def plot_performance(history):
+	acc = history.history['accuracy']
+	val_acc = history.history['val_accuracy']
+
+	loss = history.history['loss']
+	val_loss = history.history['val_loss']
+
+	epochs_range = range(epochs)
+
+	plt.figure(figsize=(8, 8))
+	plt.subplot(1, 2, 1)
+	plt.plot(epochs_range, acc, label='Training Accuracy')
+	plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+	plt.legend(loc='lower right')
+	plt.title('Training and Validation Accuracy')
+
+	plt.subplot(1, 2, 2)
+	plt.plot(epochs_range, loss, label='Training Loss')
+	plt.plot(epochs_range, val_loss, label='Validation Loss')
+	plt.legend(loc='upper right')
+	plt.title('Training and Validation Loss')
+	plt.show()
+
+plot_performance(history)
+
+score = baseline.evaluate(test_ds, verbose=0)
+print("test loss: ", score[0])
+print("test accuracy: ", score[1])
