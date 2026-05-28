66eaa6db7000f1d6a72389a04b5bffc0a41b0ade
davoleo <dav@davoleo.net>
2026-05-28 12:03:43 +0200

    evaluate.py improvments
    - Add WB algorithms
    - changed --enhance to be a list optional argument (to support multi-values)
    - --test-dir -> --degraded: also changed it to work as a boolean flag

--- master/VIPM/final_project/evaluate.py
+++ master/VIPM/final_project/evaluate.py
@@ -7,17 +7,18 @@ from torchvision import datasets
 from torchvision.transforms import v2
 
 from project_models import Baseline2
-from project_transforms import AutoGammaCorrection
+from project_transforms import *
 from project_utils import get_device
 
 ENHANCEMENTS = {
+    'wb': WhiteBalance(algorithm=WBAlgorithm.WHITE_PATCH),
     'gamma': AutoGammaCorrection(),
 }
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', required=True, help='Checkpoint name (loaded from models/<name>.pt)')
-parser.add_argument('--test-dir', default='data/test', help='Test set directory')
-parser.add_argument('--enhance', default=None, choices=list(ENHANCEMENTS),
+parser.add_argument('--degraded', action='store_true', default=False, help='Use degraded test set insteado of the clean one')
+parser.add_argument('--enhance', nargs='+', default=None, choices=list(ENHANCEMENTS),
                     help='Unsupervised enhancement applied before classification')
 args = parser.parse_args()
 
@@ -25,10 +26,19 @@ device = get_device()
 
 transform_steps = [v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]
 if args.enhance:
-    transform_steps.append(ENHANCEMENTS[args.enhance])
+    for name in args.enhance:
+        transform_steps.append(ENHANCEMENTS[name])
 transform = v2.Compose(transform_steps)
 
-test_ds = datasets.ImageFolder(args.test_dir, transform=transform)
+test_dir = ""
+if args.degraded:
+    test_dir = 'data/test_degradato'
+else:
+    test_dir = 'data/test'
+    
+print(test_dir, )
+
+test_ds = datasets.ImageFolder(test_dir, transform=transform)
 loader = torch.utils.data.DataLoader(test_ds, batch_size=64)
 
 model = Baseline2()
